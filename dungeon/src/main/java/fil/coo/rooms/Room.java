04c7f510d1521293b111818fb36f04a62069891f
Abassi Youva <youva.abassi@etudiant.univ-lille1.fr>
2018-02-14 18:05:22 +0100

    Adding the rooms package

---
+++ dungeon/src/main/java/fil/coo/rooms/Room.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fil.coo.rooms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fil.coo.entities.Monster;
+
+/**
+ * @author abassi_youva
+ *
+ */
+public class Room {
+	
+	List monsters = new ArrayList<Monster>();
+	//List items = new ArrayList<>();
+
+}
