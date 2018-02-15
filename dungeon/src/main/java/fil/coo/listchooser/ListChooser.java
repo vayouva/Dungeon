d8417e026d29c55c349dd8f0e627c7a1d63290e4
Abassi Youva <youva.abassi@etudiant.univ-lille1.fr>
2018-02-15 16:34:58 +0100

    Adding listchooser package

---
+++ dungeon/src/main/java/fil/coo/listchooser/ListChooser.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fil.coo.listchooser;
+
+import java.util.List;
+
+import fil.coo.scanner.ScannerInt;
+
+/**
+ * @author abassi_youva
+ *
+ */
+public class ListChooser {
+
+	public ListChooser() {
+		
+	}
+	
+	public <T> T choose(String question, List<T> list) {
+		
+		int i = 1;
+		for(T obj: list) {
+			System.out.println(i++ + ". " + obj);
+		}
+		
+		int value = ScannerInt.readInt(question, i);
+		
+		return (value != 0 ? list.get(value-1) : null);
+		
+	}
+}
