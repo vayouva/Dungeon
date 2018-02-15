112390d23626a46d0a306c916c11246b850efc2f
Abassi Youva <youva.abassi@etudiant.univ-lille1.fr>
2018-02-15 16:39:17 +0100

    Adding scanner package

---
+++ dungeon/src/main/java/fil/coo/scanner/ScannerInt.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package fil.coo.scanner;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * @author abassi_youva
+ *
+ */
+public class ScannerInt {
+	
+	private static final Scanner sc = new Scanner(System.in);
+	
+	public ScannerInt() {
+		
+	}
+	
+	public static int readInt(String message, int n) {
+		
+		int input = -1;
+		
+		while(input < 1 || input >= n) {
+			System.out.println(message + " (1." + (n-1) + ") ?");
+			
+			try {
+				input = sc.nextInt();
+			} catch(InputMismatchException e) {
+				sc.skip(".*");
+			}
+		}
+		
+		return input;
+	}
+
+}
